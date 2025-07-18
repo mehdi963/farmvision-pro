4adf030bf6236458bd8ef34271b22fd74fd31dbe
mehdi963 <153312713+mehdi963@users.noreply.github.com>
2025-07-18 16:18:17 +0100

    Add extracted_project/migrations/create_users_table.sql 1752851895592

---
+++ extracted_project/migrations/create_users_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
