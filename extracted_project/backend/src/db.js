faf4c2974ad4d3e7140e21bc5b23a8e13b256437
mehdi963 <153312713+mehdi963@users.noreply.github.com>
2025-07-18 16:18:49 +0100

    Add extracted_project/backend/src/db.js 1752851927384

---
+++ extracted_project/backend/src/db.js
@@ -0,0 +1,11 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  port: process.env.PGPORT || 5432,
+});
+
+module.exports = pool;
